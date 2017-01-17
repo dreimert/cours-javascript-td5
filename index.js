50b56146977f1ae23b9b9076a5c1d6fd27ab2d5a
Damien Reimert <damien.reimert@gmail.com>
2017-01-17 18:03:43 +0100

    Création de Coord.js

--- index.js
+++ index.js
@@ -30,29 +30,7 @@ const commandes = {
   }
 };
 
-class Coord {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
-  }
-
-  toString() {
-    return `${this.x},${this.y}`;
-  }
-
-  nord() {
-    return new Coord(this.x, this.y + 1);
-  }
-  sud() {
-    return new Coord(this.x, this.y - 1);
-  }
-  ouest() {
-    return new Coord(this.x - 1, this.y);
-  }
-  est() {
-    return new Coord(this.x + 1, this.y);
-  }
-}
+const Coord = require('./Coord');
 
 class Monde {
   constructor() {
